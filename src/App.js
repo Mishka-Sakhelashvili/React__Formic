9df9e80e51dfe3cf3704218183bfe07ed988677a
mishka <mishka.sakhelashvili@gmail.com>
2022-09-18 14:43:05 +0400

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Formik } from 'formik';
+import ContactForm from './components/ContactForm';
+import * as Yup from 'yup';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState();
+
+  const ErrorSchema = Yup.object().shape({
+    username: Yup.string()
+      .required('Name is required')
+      .min(2, 'too Short')
+      .max(12, 'Too Long'),
+    age: Yup.number()
+      .typeError('Age must be a number')
+      .integer('must be an integer')
+      .required('Required')
+      .min(18, 'You must be at least 18 years')
+      .max(60, 'You must be at less than 60 years'),
+    email: Yup.string()
+      .email('Invalid email')
+      .required('Required'),
+    phone: Yup.string()
+      .matches(
+        /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/,
+        'Not a Phone number'
+      )
+      .required('Required'),
+    message: Yup.string()
+      .min(5, 'Too Short!')
+      .max(100, 'Too Long!')
+      .required('Required'),
+  });
+
+  console.log(user);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container pt-5'>
+      <div className='row justify-content-sm-center pt-5'>
+        <div className='col-ms-6 shadow round pb-3'>
+          <h1 className='text-center pt-3 text-secondary'>
+            Contact Form
+          </h1>
+          <Formik
+            initialValues={{
+              username: '',
+              age: '',
+              email: '',
+              phone: '',
+              message: '',
+            }}
+            validationSchema={ErrorSchema}
+            onSubmit={(value, { resetForm }) => {
+              setUser(value);
+              resetForm();
+            }}
+            component={ContactForm}
+          />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
